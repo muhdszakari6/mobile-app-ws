c470bd135b8e181138e9cdc927c6ee9a68be0b7b
muhdszakari6 <salimzakari6@gmail.com>
2023-03-29 14:51:15 +0100

    updated database config

--- src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
@@ -72,7 +72,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
         res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
         res.addHeader("UserID", userDto.getUserId());
-//        res.getWriter().write(token);
+        res.getWriter().write(token);
     }
 
 }
